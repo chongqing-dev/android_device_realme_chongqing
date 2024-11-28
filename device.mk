3a3870b111b3d4046e73e943d89d7cfc8152eb4c
bengris32 <bengris32@protonmail.ch>
2024-11-28 09:18:44 -0500

    chongqing: Build mediatek vibrator
    Change-Id: Id12ebb1b5aa98912eeab841847d3410a3a85b9ee
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- device.mk
+++ device.mk
@@ -124,5 +124,9 @@ PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.mediatek
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/chongqing/chongqing-vendor.mk)
