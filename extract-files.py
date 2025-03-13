06c358cb44d7f8d8c1605906347ca7b72c1b3e3c
ChrisCatto <chriscatto@proton.me>
2025-03-13 17:30:55 +0200

    chonqing: Fix elf checks for `libneuralnetworks_sl_driver_mtk_prebuilt`
    Change-Id: I13570372250ec0fd3ed4184f5a8ae58d4aabd6fb
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,14 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00'),
     'vendor/lib64/libmnl.so': blob_fixup()
         .add_needed('libcutils.so'),
+    'vendor/lib64/mt6835/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
